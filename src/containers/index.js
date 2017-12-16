1197d44ef1f0a4e90f6a7f4ee0deb454f5ef6660
gangchang <ktw6750@hanmail.net>
2017-12-16 23:19:00 +0900

    containers index add

---
+++ src/containers/index.js
@@ -0,0 +1,15 @@
+import HomeContainer from './HomeContainer';
+import SearchResultContainer from './SearchResultContainer';
+import MovieDetailContainer from './MovieDetailContainer';
+import BoxOfficeContainer from './BoxOfficeContainer';
+import CategoryContainer from './CategoryContainer';
+import HeaderContainer from './HeaderContainer';
+
+export {
+    HomeContainer,
+    SearchResultContainer,
+    MovieDetailContainer,
+    BoxOfficeContainer,
+    CategoryContainer,
+    HeaderContainer
+};
