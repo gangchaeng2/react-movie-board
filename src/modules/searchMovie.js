1f10278af7c83808c9cb0c921c6eaf0622eb8f0b
gangchang <ktw6750@hanmail.net>
2017-12-29 00:44:58 +0900

    boxOffice update

--- src/modules/searchMovie.js
+++ src/modules/searchMovie.js
@@ -12,16 +12,12 @@ function searchMovieListByQuery(query, page) {
 const GET_MOVIE = 'GET_MOVIE';
 const SET_QUERY = 'SET_QUERY';
 const SET_PAGE = 'SET_PAGE'
-/* redux-pender 의 액션 구조는 Flux standard action(https://github.com/acdlite/flux-standard-action)
-   을 따르기 때문에, createAction 으로 액션을 생성 할 수 있습니다. 두번째로 들어가는 파라미터는 프로미스를 반환하는
-   함수여야 합니다.
-*/
+
 export const searchMovie = createAction(GET_MOVIE, searchMovieListByQuery);
 export const setQuery = createAction(SET_QUERY);
 export const setPage = createAction(SET_PAGE);
 
 const initialState = {
-    // 요청이 진행중인지, 에러가 났는지의 여부는 더 이상 직접 관리 할 필요가 없어집니다. penderReducer 가 담당하기 때문이죠
     items: [],
     query: '',
     page: 1,
@@ -79,6 +75,6 @@ export default handleActions({
                 loadingStatus: false
             }
         }
-        // 함수가 생략됐을때 기본 값으론 (state, action) => state 가 설정됩니다 (state 를 그대로 반환한다는 것이죠)
+        // 함수가 생략됐을때 기본 값으론 (state, action) => state 가 설정됩니다. 
     })
 }, initialState);
